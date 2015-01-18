475c843a0c4d48db97efddea2b8c136bcd24ae48
Richard Hansen <rhansen@bbn.com>
2015-01-18 15:51:41 -0500

    basic structure
      * create mostly empty package/modules
      * create main executable
      * empty main() function

---
+++ fasguard_benign_traffic_collection/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2015 Raytheon BBN Technologies Corp.  All rights reserved.
+
+from __future__ import absolute_import
+
+import sys
+
+def main(raw_args=sys.argv):
+    raise NotImplementedError()
---
+++ fasguard_benign_traffic_collection/util.py
@@ -0,0 +1,3 @@
+# Copyright (c) 2015 Raytheon BBN Technologies Corp.  All rights reserved.
+
+from __future__ import absolute_import
